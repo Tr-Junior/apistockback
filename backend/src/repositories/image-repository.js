946ea9dafea2b9046eb7044aeadbdae21b742b47
Tr-junior <tarcisio.junior1406@gmail.com>
2025-01-23 00:36:15 -0300

    update image-repository.js

--- backend/src/repositories/image-repository.js
+++ backend/src/repositories/image-repository.js
@@ -19,8 +19,10 @@ exports.deleteImageByType = async (type) => {
   return res;
 };
 
-exports.get = async (type) => {
+exports.getImageByType = async (type) => {
   const query = type ? { type } : {};  // Se 'type' for passado, filtra pela propriedade 'type'
-  const images = await Image.find(query);  // Faz a busca no banco com o filtro
+  const images = await Image.findOne(query);  // Faz a busca no banco com o filtro
   return images;
 };
+
+
