fc36a190c32066e8a64d01df911d28b8fbc0672d
Trjunior <tarcisio.junior1406@gmail.com>
2025-01-24 19:02:07 -0300

    update upload-config.js

--- backend/src/services/upload-config.js
+++ backend/src/services/upload-config.js
@@ -2,27 +2,28 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// Verificar e criar o diretório de uploads, caso não exista
+// Definição do diretório de uploads
 const uploadDir = path.resolve(process.env.UPLOAD_DIR || './src/uploads');
 
+// Verifica e cria a pasta se não existir
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+// Configuração do armazenamento
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);  // Usar o diretório absoluto
+    cb(null, uploadDir);  // Diretório absoluto para armazenar os arquivos
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);  // Nome único para o arquivo
+    cb(null, `${Date.now()}-${file.originalname}`);  // Garante um nome único
   }
 });
-
 
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
   if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);  // Arquivo permitido
+    cb(null, true);
   } else {
     cb(new Error('Arquivo inválido! Apenas imagens são permitidas'), false);
   }
@@ -31,7 +32,8 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 }  // Limite de tamanho (5MB)
+  limits: { fileSize: 10 * 1024 * 1024 }  // Limite de tamanho: 10MB
 });
 
 module.exports = upload;
+module.exports.uploadDir = uploadDir;
