f8518f84c0ff9280cf05d753b7935c5ef2462410
Trjunior <tarcisio.junior1406@gmail.com>
2025-01-24 19:02:04 -0300

    update image-repository.js

--- backend/src/repositories/image-repository.js
+++ backend/src/repositories/image-repository.js
@@ -1,28 +1,24 @@
-const mongoose = require('mongoose');
-const Image = mongoose.model('Image');
+const Image = require('../models/image');
 
-exports.createImage = async (type, filePath) => {
-  const image = new Image({ type, filePath });
+exports.createImage = async (type, filePath, name, imageUrl) => {
+  const image = new Image({ type, filePath, name, imageUrl });
   await image.save();
   return image;
 };
 
-
-
-exports.updateImage = async (type, filePath) => {
-  const updatedImage = await Image.findOneAndUpdate({ type }, { filePath }, { new: true });
-  return updatedImage;
+exports.updateImage = async (type, filePath, name, imageUrl) => {
+  return await Image.findOneAndUpdate(
+    { type },
+    { filePath, name, imageUrl },
+    { new: true }
+  );
 };
 
+
 exports.deleteImageByType = async (type) => {
-  const res = await Image.deleteOne({ type });
-  return res;
+  return await Image.deleteOne({ type });
 };
 
 exports.getImageByType = async (type) => {
-  const query = type ? { type } : {};  // Se 'type' for passado, filtra pela propriedade 'type'
-  const images = await Image.findOne(query);  // Faz a busca no banco com o filtro
-  return images;
+  return await Image.findOne({ type });
 };
-
-
