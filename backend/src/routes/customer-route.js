4ee157a389e8061d501462c489682f5efb7663b6
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-11 10:47:11 -0300

    add route to update admin credentials after first login

--- backend/src/routes/customer-route.js
+++ backend/src/routes/customer-route.js
@@ -4,12 +4,19 @@ const controller = require('../controllers/customer-controller');
 const authService = require('../services/auth-service');
 const customerMiddleware = require('../middlewares/customer-middleware');
 
+// Rotas protegidas para administração
 router.post('/', authService.isAdmin, customerMiddleware.validateCustomerData, controller.post);
 router.get('/', authService.isAdmin, controller.get);
 router.get('/check-username/:name', authService.isAdmin, controller.getByName);
+
+// Rotas de autenticação
 router.post('/authenticate', controller.authenticate);
 router.post('/refresh-token', authService.authorize, controller.refreshToken);
 router.get('/getById/:id', authService.authorize, controller.getById);
 router.put('/update-password', authService.authorize, controller.updatePassword);
 router.post('/validate-password', controller.validatePassword);
+
+// Rota para atualizar credenciais do admin após o primeiro login
+router.post('/update-admin', authService.authorize, controller.updateAdminCredentials);
+
 module.exports = router;
