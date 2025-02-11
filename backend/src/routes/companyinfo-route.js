06541f1bf293d319da72a6555c845043c2e9e71d
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-11 10:47:15 -0300

    add admin authentication to company info routes

--- backend/src/routes/companyinfo-route.js
+++ backend/src/routes/companyinfo-route.js
@@ -2,13 +2,14 @@ const express = require("express");
 const controller = require("../controllers/companyInfo-controller");
 const companyInfoMiddleware = require("../middlewares/companyInfo-middleware");
 const router = express.Router();
+const authService = require('../services/auth-service');
 
 
-router.post("/company", companyInfoMiddleware.validateCompanyInfo, controller.createCompanyInfo);
-router.get("/company", controller.getAllCompanyInfo);
-router.get("/company/:id", controller.getCompanyInfoById);
-router.put("/company/:id", controller.updateCompanyInfo);
-router.delete("/company/:id", controller.deleteCompanyInfo);
+router.post("/company",authService.isAdmin, companyInfoMiddleware.validateCompanyInfo, controller.createCompanyInfo);
+router.get("/company",authService.isAdmin, controller.getAllCompanyInfo);
+router.get("/company/:id",authService.isAdmin, controller.getCompanyInfoById);
+router.put("/company/:id",authService.isAdmin, controller.updateCompanyInfo);
+router.delete("/company/:id",authService.isAdmin, controller.deleteCompanyInfo);
 
 
 module.exports = router;
