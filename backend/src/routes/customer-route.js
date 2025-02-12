eb356ca672660bd4ecd7a3ee5401db2ac488895b
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-12 18:10:20 -0300

    add authorization to password validation and update admin routes; implement delete user functionality

--- backend/src/routes/customer-route.js
+++ backend/src/routes/customer-route.js
@@ -12,8 +12,9 @@ router.post('/authenticate', controller.authenticate);
 router.post('/refresh-token', authService.authorize, controller.refreshToken);
 router.get('/getById/:id', authService.authorize, controller.getById);
 router.put('/update-password', authService.authorize, controller.updatePassword);
-router.post('/validate-password', controller.validatePassword);
+router.post('/validate-password',authService.authorize, controller.validatePassword);
+router.delete('/delete-user/:id',authService.isAdmin, controller.deleteUser)
 
-router.post('/update-admin', authService.authorize, controller.updateAdminCredentials);
+router.post('/update-admin', authService.isAdmin, controller.updateAdminCredentials);
 
 module.exports = router;
