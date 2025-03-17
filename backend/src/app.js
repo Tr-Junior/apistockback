c65116b48ea35dcce1593435f2686632629e53e1
Trjunior <tarcisio.junior1406@gmail.com>
2025-03-17 17:51:13 -0300

    add helmet for security and implement path validation middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const helmet = require("helmet"); // Importando o Helmet
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const router = express.Router();
 const cors = require('cors');
 require("dotenv").config();
 const errorMiddleware = require("./middlewares/errorMiddleware");
 const path = require('path');
 const fs = require('fs');
+const validatePathMiddleware = require("./middlewares/validatePathMiddleware");
+
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
-    
+
 const corsOptions = {
-  origin: '*', // Permitir apenas esta origem
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-access-token'], // Cabeçalhos permitidos
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-access-token'], 
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json(
-    { limit: '5mb' }
-));
+app.use(helmet()); // Aplicando Helmet para segurança
+app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Importação de Modelos
 const Product = require('./models/product');
 const Customer = require('./models/customer');
 const Order = require('./models/order');
@@ -38,8 +41,7 @@ const Supplier = require('./models/supplier');
 const CompanyInfo = require('./models/companyInfo');
 const Image = require('./models/image');
 
-
-
+// Importação de Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
@@ -53,7 +55,10 @@ const cepRoute = require('./routes/cep-route');
 const companyInfoRoute = require("./routes/companyinfo-route");
 const imageRoute = require("./routes/image-route");
 
+// Middleware de validação de caminhos
+app.use(validatePathMiddleware);
 
+// Aplicando Rotas
 app.use('/', indexRoute);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
@@ -67,17 +72,15 @@ app.use('/companyInfo', companyInfoRoute);
 app.use('/image', imageRoute);
 app.use("/api", cepRoute);
 
-
+// Middleware de Erro
 app.use(errorMiddleware);
-//app.use(uploadMiddleware);
-
 
 // Servir arquivos estáticos da pasta 'uploads'
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Rota para SPA (Single Page Application)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 
 module.exports = app;
